158e3cea9a0f54fa4fadba5fdc8f32e44d509ce0
Mayank <mkhadotkar623@gmail.com>
2021-01-12 11:11:34 +0530

    Array added in testMedicine class and output for animal and medcine are added

--- ELTP Assignments java/src/Medicines/testMedicine.java
+++ ELTP Assignments java/src/Medicines/testMedicine.java
@@ -1,27 +1,37 @@
 package Medicines;
 
+import java.util.Random;
+
 public class testMedicine {
 
 	public static void main(String[] args) {
+	
+		medicine arr[]=new medicine[10];
+		Random r=new Random();
+		for (int i = 0; i < arr.length; i++) {
+			
+			int temp=r.nextInt(3);
+			if(temp==0)
+			{
+				arr[i]=new tablet("TabShop", "Dry Area Market", "Solid", "Kept in dry area");
+				
+			}
+			else if(temp==1)
+			{
+				arr[i]=new syrup("SyrupStore", "Center Market", "Liquid", "Kept in any area");
+			}
+			else 
+			{
+				arr[i]=new ointment("OitnmentShop", "Outside Market", "Oil", "For external use");
+			}
+		}
 		
-		//----------------object of medicine class-----------------
-		medicine m=new medicine("MediShop","In market");
-		m.displayLab();
-		
-		//---------------object of tablet class----------------
-		System.out.println("=================================");
-		tablet t=new tablet("TabShop", "Dry Area Market", "Solid", "Kept in dry area");
-		t.displayLab();
-		
-		//--------------object of syrup class---------------
-		System.out.println("=================================");
-		syrup s=new syrup("SyrupStore", "Center Market", "Liquid", "Kept in any area");
-		s.displayLab();
-		
-		//---------------object of ointment class-------------
-		System.out.println("=================================");
-		ointment o=new ointment("OitnmentShop", "Outside Market", "Oil", "For external use");
-		o.displayLab();
+		for (int i = 0; i < arr.length; i++) {
+			
+			System.out.println("---------------------------------------");
+			arr[i].displayLab();
+			
+		}
 		
 		
 		
