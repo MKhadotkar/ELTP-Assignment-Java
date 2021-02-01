27ea37a921a9ffc49984bf6d6f500139521066b1
Mayank <mkhadotkar623@gmail.com>
2021-02-01 10:27:49 +0530

    collection and exception handling porjects are added

---
+++ ELTP Assignments java/src/collection/SortByDuration.java
@@ -0,0 +1,11 @@
+package collection;
+
+import java.util.Comparator;
+
+public class SortByDuration implements Comparator<Movie>{
+	@Override
+	public int compare(Movie o1, Movie o2) {
+		// TODO Auto-generated method stub
+		return o1.getDuration().compareTo(o2.getDuration());
+	}
+}
